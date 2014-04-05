8739e7981e12e514318bb6a117b2a6f899457970
Carlos Zubieta <czubieta.dev@gmail.com>
2014-04-05 00:56:44 -0600

    Deputies party retrieving + bug fix

--- DeputyScraper/spiders/deputies_spider.py
+++ DeputyScraper/spiders/deputies_spider.py
@@ -8,7 +8,14 @@ base_dir    = "Deputies/"
 base_file   = "deputies"
 filename    = base_dir+base_file
 # TODO: find a way to retrieve the parties without hardcoding the party logos
-parties_logos = ["pri01.png", "pan.png", "prd01.png", "logvrd.jpg", "logo_movimiento_ciudada", "logpt.jpg", "panal.gif"]
+parties_logos = {"pri01.png" : "pri",
+                 "pan.png"   : "pan",
+                 "prd01.png" : "prd",
+                 "logvrd.jpg": "verde",
+                 "logo_movimiento_ciudadano.png" : "movimiento ciudadano",
+                 "logpt.jpg" : "pt",
+                 "panal.gif":"panal" 
+                 }
 
 class DiputadosSpider(Spider):
     name = "deputies"
@@ -19,22 +26,26 @@ class DiputadosSpider(Spider):
         open(filename+".html", 'wb').write(responce.body)
         sel = Selector(responce)
 
-        deputies_fields = sel.xpath('//td[@class="EncabezadoVerde"]/ancestor::table[1]//tr/td[@class="textoNegro"]/a')
+        deputies_fields = sel.xpath('//td[@class="EncabezadoVerde"]/\
+            ancestor::table[1]//tr/td[@class="textoNegro"]/a')
         
         deputies_names  = deputies_fields.xpath('text()').extract()
         deputies_links  = deputies_fields.xpath('@href').extract()
         deputies_meta   = deputies_fields.xpath('ancestor::td[1]')
         deputies_state  = deputies_meta.xpath('following-sibling::*[1]/text()').extract()
         deputies_district = deputies_meta.xpath('following-sibling::*[2]/text()').extract()
-# TODO: we really need a better way to retrieve the deputy's party  
-        deputies_party  = deputies_meta.xpath('ancestor::tr[1]/preceding-sibling::*/td/img[@src!="images/h_line.gif"]/@src')
-
-        print(len(deputies_party))
+        # TODO: we really need a better way to retrieve the deputy's party 
+        deputies_logos = deputies_meta.xpath('ancestor::tr[1]/\
+            preceding-sibling::*[./td/img/@src!="images/h_line.gif"][1]/\
+            td/img/@src').extract()
+        deputies_party = [parties_logos[logo.split('/')[-1]] for logo in deputies_logos]
 
         file = codecs.open(filename+'_data', 'w', 'utf_8')
         for i in range(len(deputies_names)):
-            file.write(' '.join(deputies_names[i].split()[1:])+'\t'+
-                base_url+deputies_links[i]+'\t'+
-                deputies_state[i]+'\t'+
-                deputies_district[i]+'\n')
-        file.close()
+            file.write(' '.join(deputies_names[i].split()[1:]) + '\t' + 
+                base_url+deputies_links[i] + '\t' +
+                deputies_state[i] + '\t' + 
+                deputies_district[i] + '\t' +
+                deputies_party[i] + '\n'
+                )
+#            file.close()
